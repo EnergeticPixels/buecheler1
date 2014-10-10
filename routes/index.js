73588adeed158a2a907fc62f3dc601e6bd0cd2ae
Anthony Jackman <energeticpixels@gmail.com>
2014-10-09 22:21:21 -0500

    added caps to add data to database, corrected submit form user.email. end of tutorial CW Buelecheler

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,38 @@ router.get('/userlist', function (req, res) {
 	});
 });
 
+/* here is how we get data into our db collection */
+router.post('/adduser', function (req, res) {
+	// set our internal db variable
+	var db = req.db;
+	// get our form values. these rely on the name attributes
+	var userName = req.body.username;
+	var userEmail = req.body.useremail;
+	// set our collectoin
+	var collection = db.get('usercollection');
+	// submit to the db
+	collection.insert({
+		"username": userName,
+		"email": userEmail
+	}, function (err, doc) {
+		if (err) {
+			// if it failed return error
+			res.send("there was a problem adding the information to the database.")
+		}
+		else {
+			// if it worked, set the header so the address bar doesn't still say /adduser
+			res.location("userlist");
+			// add forward to success page
+			res.redirect("userlist");
+		}
+	});
+});
+
+
+/* adding a route so that we can input data to database through webpage */
+/* GET New User page. */
+router.get('/newuser', function (req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
 
 module.exports = router;
